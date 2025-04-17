3afed8f193444a9136edebe1df6bf191082cbabc
David Kiptum <davy@DESKTOP-7A71LJO.>
2025-04-17 13:55:19 +0300

    adds shopping lists

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,22 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Item from "./Item";
+import items from "../data/items"; // assuming you're importing your items array here
 
-function ShoppingList({ items }) {
+function ShoppingList() {
+  const [selectedCategory, setSelectedCategory] = useState("All");
+
+  function handleChange(event) {
+    setSelectedCategory(event.target.value);
+  }
+
+  const itemsToDisplay = items.filter((item) => {
+    return selectedCategory === "All" || item.category === selectedCategory;
+  });
+
   return (
     <div className="ShoppingList">
-      <div className="Filter">
-        <select name="filter">
-          <option value="All">Filter by category</option>
-          <option value="Produce">Produce</option>
-          <option value="Dairy">Dairy</option>
-          <option value="Dessert">Dessert</option>
-        </select>
-      </div>
+      <Filter
+        selectedCategory={selectedCategory}
+        onCategoryChange={handleChange}
+      />
       <ul className="Items">
-        {items.map((item) => (
+        {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
         ))}
       </ul>
+    </div>
+  );
+}
+
+function Filter({ selectedCategory, onCategoryChange }) {
+  return (
+    <div className="Filter">
+      <select
+        name="filter"
+        value={selectedCategory}
+        onChange={onCategoryChange}
+      >
+        <option value="All">Filter by category</option>
+        <option value="Produce">Produce</option>
+        <option value="Dairy">Dairy</option>
+        <option value="Dessert">Dessert</option>
+      </select>
     </div>
   );
 }
